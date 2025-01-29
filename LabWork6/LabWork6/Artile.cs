132d1582443d43341b720b1e426f3296d033033a
Miyamoto1701 <artemlichkov@gmail.com>
2025-01-29 10:08:33 +0300

    Add files via upload

---
+++ LabWork6/LabWork6/Artile.cs
@@ -0,0 +1,27 @@
+﻿public class Article
+{
+    private string title;
+    private string author;
+    private int year;
+
+    public Article()
+    {
+        title = "без названия";
+        author = "без автора";
+        year = 0;
+    }
+
+    public Article(string title, string author, int year)
+    {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Название: {title}");
+        Console.WriteLine($"Имя автора: {author}");
+        Console.WriteLine($"Год публикации {year}");
+    }
+}
---
+++ LabWork6/LabWork6/Program.cs
@@ -0,0 +1,5 @@
+﻿Article defaultArticle = new Article();
+defaultArticle.GetInfo();
+Console.WriteLine();
+Article parametrArticle = new Article("How to block Uskov","Artem Luchkov",2023);
+parametrArticle.GetInfo();
